3875a66361ad23ab71fa921ae5fc6630a37e4804
LShemetov <35998656+LShemetov@users.noreply.github.com>
2018-02-20 20:13:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  $file = '/tmp/sample-app.log';
+  $message = file_get_contents('php://input');
+  file_put_contents($file, date('Y-m-d H:i:s') . " Received message: " . $message . "\n", FILE_APPEND);
+}
+else
+{
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Project for subject Infomation Security</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
+    <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="/styles.css" type="text/css">
+</head>
+<body>
+    <section class="congratulations">
+        <h1>Uryvaev Kirill!</h1>
+        <p>Student of group 8V4A</p>
+        <p>This site is running on PHP <?= phpversion() ?></p>
+    </section>
+
+    <section class="instructions">
+        <h2>Here are usefull information</h2>
+        <ul>
+            <li><a href="https://www.intuit.ru/">Intuit courses</a></li>
+            <li><a href="http://video.lisa-alisa.ru/">Usefull video</a></li>
+            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_PHP.rds.html">Coursera courses</a>
+            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/customize-containers-ec2.html">Information security tests</a></li>
+            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/customize-containers-resources.html">Advices</a></li>
+        </ul>
+
+        <h2>PHP manuals</h2>
+        <ul>
+            <li><a href="http://aws.amazon.com/sdkforphp">PHP manual #1</a></li>
+            <li><a href="http://aws.amazon.com/php">PHP manual #2</a></li>
+            <li><a href="https://github.com/aws/aws-sdk-php">PHP manual #3</a></li>
+        </ul>
+    </section>
+
+    <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
+</body>
+</html>
+<? 
+} 
+?>
